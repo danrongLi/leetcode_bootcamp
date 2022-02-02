cc6aaa04bb900901bd985ef58f08510299908636
danrongli <dl3333@columbia.edu>
2022-02-02 18:46:11 -0500

    week 1

---
+++ week1/977.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+//         merge sort/ quick sort ---> O(nlogn) brute force
+//         the following method: O(n)
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+
+        vector<int> ans(n);
+        for(int i = n-1; i >= 0; i -=1){
+            int temp;
+            if(abs(nums[left]) < abs(nums[right])){
+                temp = nums[right];
+                right -= 1;
+            }
+            else {
+                temp = nums[left];
+                left += 1;
+            }
+            ans[i] = temp * temp;
+        }
+        return ans;
+    }
+
+};
